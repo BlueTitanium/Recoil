5278e7a1e94ca031f835ea2a88dadbcab249dfd0
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2023-02-05 20:48:52 -0500

    turret

--- Recoil/Assets/Scripts/DamageTurretAI.cs
+++ Recoil/Assets/Scripts/DamageTurretAI.cs
@@ -6,7 +6,7 @@ public class DamageTurretAI : MonoBehaviour
 {
      public GameObject bulletPrefab;
      public Transform spawnPoint;
-     
+
     // Start is called before the first frame update
     void Start()
     {
