e6b135c3b7348975831241f78d55cd18e50457d9
Ziynou Fleming <98552523+Zighnou@users.noreply.github.com>
2023-02-05 20:04:22 -0500

    created slimeAI

---
+++ Recoil/Assets/Scripts/SlimeAI.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeAI : MonoBehaviour
+{
+    public GameObject explosion;
+    Transform player;
+    public Transform castPoint;
+    Animator _animator;
+    Rigidbody2D _rigidbody;
+    
+    public LayerMask groundLayer;
+    public LayerMask wallLayer;
+    bool isGrounded;
+    bool isWall;
+    float groundCheckDist = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        StartCoroutine(SlimeMove());
+        
+    }
+
+    private void FixedUpdate() 
+    {
+        isGrounded = Physics2D.OverlapCircle(castPoint.position, groundCheckDist, groundLayer);    
+        isWall = Physics2D.OverlapCircle(castPoint.position, groundCheckDist, wallLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Flips sprite if not grounded / hitting wall
+        if (!isGrounded || isWall)
+        {
+            transform.localScale *= new Vector2(-1, 1);
+        }
+
+        // Flips sprite to face player direction
+        // else if (player.position.x > transform.position.x && transform.localScale.x < 0 ||
+        // player.position.x < transform.position.x && transform.localScale.x > 0) 
+        // {
+        //     transform.localScale *= new Vector2(-1,1);
+        // }
+    }
+
+    IEnumerator SlimeMove()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1f, 5f));
+            _rigidbody.AddForce(new Vector2(transform.localScale.x * 100, 100));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            // Instantiate(explosion, transform.position, Quarternion.identity);
+            Destroy(other.gameObject);
+            // _animator.SetTrigger("Die");
+            Destroy(gameObject, .15f);
+        }
+        
+    }
+}
